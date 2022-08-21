be424c40978d8abe687385e36578148e31a4cd34
misraerol <misraerol@gmail.com>
2022-08-21 18:58:53 +0300

    Create Layers, Add Migration

--- src/demoProjects/rentACar/Domain/Entities/Brand.cs
+++ src/demoProjects/rentACar/Domain/Entities/Brand.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Core.Persistence.Repositories;
 
 namespace Domain.Entities
 {
-    internal class Brand
+    public class Brand:Entity
     {
+        public string Name { get; set; }
+
+        public Brand()
+        {
+        }
+
+        public Brand(int id,string name):this()
+        {
+            Id = id;
+            Name = name;
+        }
     }
 }
--- src/demoProjects/rentACar/Persistence/Migrations/20220821155424_Init.Designer.cs
+++ src/demoProjects/rentACar/Persistence/Migrations/20220821155424_Init.Designer.cs
@@ -11,7 +11,7 @@ using Persistence.Contexts;
 namespace Persistence.Migrations
 {
     [DbContext(typeof(BaseDbContext))]
-    [Migration("20220815205354_Init")]
+    [Migration("20220821155424_Init")]
     partial class Init
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
